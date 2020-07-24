28dd69be8cfd0177852c908ba1c6f934263b8c61
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-24 16:47:54 -0400

    added api connection for random user generator

---
+++ src/utils/API.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default {
+    employeeInfo: function () {
+        return axios.get('https://randomuser.me/api/?results=100&inc=name,phone,nat,email,picture,dob&nat=us');
+    }
+};
