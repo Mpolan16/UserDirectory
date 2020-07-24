6ed1ee333d83093230db2150003ee2caa8d625c8
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-24 18:06:57 -0400

    added SearchArea and Table Area into main to better organize the info

--- src/components/EmployeeSearch/index.js
+++ src/components/EmployeeSearch/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import'./style.css';
 
 function EmployeeSearch(){
+    return
     <div>
-        
+        {/* handles the search for employee */}
     </div>
 }
 export default EmployeeSearch;
