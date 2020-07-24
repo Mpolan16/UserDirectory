a68fb09fac9ef443a11d6e0e6ef45c507fb1a6ba
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-24 19:22:41 -0400

    added tables and was able to connect API to show employee info

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default {
     employeeInfo: function () {
-        return axios.get('https://randomuser.me/api/?results=50&inc=name,email,dob,phone,picture&nat=us');
+        return axios.get('https://randomuser.me/api/?results=20&inc=id,name,email,dob,phone,picture&nat=us');
     }
 };
