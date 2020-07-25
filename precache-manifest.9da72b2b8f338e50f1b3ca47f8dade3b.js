5a7e00bdbc99ee37ca30c2f8583b4729db3e3929
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-25 13:45:20 -0400

    Updates

---
+++ precache-manifest.9da72b2b8f338e50f1b3ca47f8dade3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c84b2efa454bf34a7380736431284e0",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "b07a2a7c6e362ed93f1f",
+    "url": "/employee-directory/static/css/main.3aecbae8.chunk.css"
+  },
+  {
+    "revision": "010866e6ce1858769151",
+    "url": "/employee-directory/static/js/2.c7f7d248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.c7f7d248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07a2a7c6e362ed93f1f",
+    "url": "/employee-directory/static/js/main.4472d428.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
