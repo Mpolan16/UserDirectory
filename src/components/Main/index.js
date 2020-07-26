2e53165a741d1c25366592853aed89cdade8314a
Mónica Polanco <mpolan16@g.holycross.edu>
2020-07-25 21:13:46 -0400

    fixed bugs in Main folder

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -24,27 +24,27 @@ export default class Main extends Component {
         let nameSeach = event.target.value;
         let empLookingFor = this.state.employees.filter (foundEmp => {
             return(
-                foundEmp.lastName.toLowercase().includes(nameSeach.toLowercase())
+                foundEmp.lastName.toLowerCase().includes(nameSeach.toLowerCase())
             )
         })
         this.setState({employees:empLookingFor})
     }
     sortLastName = () =>{
 // add state to say what the current state of function (A and D order)..use if else, sort function pass it a compare function inside the sort function.
-        if (this.state.sortStatus ==="D") {
+        if (this.state.sortStatus === "D") {
             let ascendingEmp = this.state.employees.sort((a,b) => a.lastName > b.lastName ? 1: -1)
 
             this.setState({
-                employees:ascendingEmp,
-                sortStatus: "A",
+                employees: ascendingEmp,
+                sortStatus: "A"
             });
         } else{
             let descendingEmp = this.state.employees.sort((a,b) => a.lastName < b.lastName ? 1: -1)
 
             this.setState({
-                employees:descendingEmp,
-                sortStatus:"D",
-            })
+                employees: descendingEmp,
+                sortStatus:"D"
+            });
         }
     }
     render() {
